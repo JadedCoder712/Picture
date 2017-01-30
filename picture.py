3c9be0f9290dbcb2ce52a2fd25f187769e6e9005
JadedCoder712 <thomaspostans@hanovernorwichschools.org>
2017-01-30 14:56:18 -0500

    Update picture.py

--- picture.py
+++ picture.py
@@ -1,7 +1,7 @@
 """
 picture.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Kyle Postans
+Credit: none ever
 
 Assignment:
 
@@ -27,6 +27,36 @@ for detailed information on ggame.
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
 # add your code here \/  \/  \/
+rot = Color(0xff0000, 0.8)
+grun = Color(0x00ff00, 1.0)
+blau = Color(0x0000ff, 1.0)
+schwartz = Color(0x000000, 1.0)
+orange = Color(0xffa500, 1.0)
+gelb = Color(0xffff00, 0.7)
+
+porklein = LineStyle (1, schwartz)
+
+brick = RectangleAsset(190, 68, porklein, rot) #brick
+lolrundiscircle = CircleAsset(68, porklein, orange) #face
+lolelipticalpooltable = EllipseAsset(65, 20, porklein, gelb)
+eyes = CircleAsset(10, porklein, schwartz) #eyes
+mouth = CircleAsset(13, porklein, rot)
+
+Sprite(brick, (300, 300))
+Sprite(brick, (490, 300))
+Sprite(brick, (680, 300))
+Sprite(brick, (290, 368))
+Sprite(brick, (480, 368))
+Sprite(brick, (670, 368))
+Sprite(lolrundiscircle, (190, 190))
+Sprite(lolelipticalpooltable, (175, 126))
+Sprite(eyes, (170, 170))
+Sprite(eyes, (210, 170))
+Sprite(mouth, (190, 210))
+
+
+
+
 
 
 # add your code here /\  /\  /\
